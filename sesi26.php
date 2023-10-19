764c2cfb1a3e24508237dd714335b54798b670db
Mardhotillah12 <mardhotillah551@gmail.com>
2023-10-19 20:39:22 +0700

    Tambah Tugas File Sesi 26

---
+++ sesi26.php
@@ -0,0 +1,33 @@
+<?php
+
+// Nomor 1
+
+for ($i = 1; $i <= 10; $i++) {
+    if ($i % 2 == 0) {
+        echo "Angka $i adalah bilangan genap<br>";
+    } else {
+        echo "Angka $i adalah bilangan ganjil<br>";
+    }
+}
+
+// Nomor 2
+echo "<br>";
+for ($tahun = 2000; $tahun <= 2023; $tahun++) {
+    if (($tahun % 4 == 0 && $tahun % 100 != 0) || $tahun % 400 == 0) {
+        echo "$tahun adalah tahun kabisat<br>";
+    } else {
+        echo "$tahun bukan tahun kabisat<br>";
+    }
+}
+
+// Nomor 3
+echo "<br>";
+
+for ($x = 9; $x >= 1; $x--) {
+    for ($y = 1; $y <= $x; $y++) {
+        echo $y;
+    }
+    echo "<br>";
+}
+
+?>
